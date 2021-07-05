95d49dcc896cad6eac2e1c48eb3af09a41fc4e8d
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-07-05 19:59:17 +0200

    added window printing with curses

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
 #!/usr/bin/python3
-from pprint import pprint
-from symbols import symbols
+# TODO: make it work with colors
+import curses
 import math
-from math import cos, sin
+import os
+import sys
 from copy import deepcopy
-class Cube():
-    def __init__(self, s = 17):
-        # assert s % 4 == 0
+from math import cos, sin
+from pprint import pprint
+
+from symbols import symbols
+
+
+class Cube:
+    def __init__(self, s=17):
         self.rotation = -15
         self.s = s
-        # self.cube = Cube.create_cube(s)
-        # Cube.print_rows(self.proyect())
-        # new_points = self.rotate(45)
-        # self.cube = Cube.create_cube(s)
-        # self.write_new_points(new_points)
-        # proyect = self.proyect() 
-        # Cube.print_rows(proyect)
-        # Cube.print_proyection(proyect)
-        # self.connect_all()
-        # proyect = self.proyect() 
-        # Cube.print_proyection(proyect)
-        for _ in range(10):
-            self.rotate_and_print()
+        self.screen = curses.initscr()
+        while 1:
+            try:
+                self.rotate_and_print()
+            except KeyboardInterrupt:
+                curses.endwin()
+                sys.exit()
 
     def rotate_and_print(self):
         self.cube = Cube.create_cube(self.s)
         self.cube = self.write_cube(self.cube)
-        new_points = self.rotate(self.rotation + 15)
+        inc = 3
+        new_points = self.rotate(
+            self.rotation + inc, self.rotation + inc, self.rotation + inc
+        )
         self.cube = Cube.create_cube(self.s)
         self.write_new_points(new_points)
         self.connect_all()
         proyection = self.proyect_with_depth()
-        Cube.print_proyection_with_depth(proyection)
-        print("---------------------------------------------------")
+        proyection = Cube.get_proyection_string(proyection)
+        self.window_print(proyection)
+
+    def window_print(self, proyection):
+        self.screen.clear()
+        self.screen.addstr(proyection)
+        self.screen.refresh()
+        curses.napms(1)
 
     def create_cube(s):
         return [[[0 for _ in range(s)] for _ in range(s)] for _ in range(s)]
+
     def write_cube(self, cube):
-        s = self.s / 2 # Maybe this needs a round
+        s = self.s / 2  # Maybe this needs a round
         long = math.ceil(int(s / 2 + s))
         short = math.floor(int(s / 2))
         # long = round(int(s / 2 + s))
@@ -71,7 +81,6 @@ class Cube():
         i = 0
         points = list(points)
         x, y, z = point
-        # results = []
         distances = []
         for p in points:
             x1, y1, z1 = p
@@ -98,7 +107,35 @@ class Cube():
         assert len(results) == 3
         return results
 
-    def connect(point1: tuple, point2: tuple):
+    def connect3d(point1, point2, new_point):
+        # Formula new_point = point1 + t * point2
+        x, y, z = new_point
+        # TODO: rewrite this is ugly
+        if (
+            (x != None and y != None)
+            or (x != None and z != None)
+            or (y != None and z != None)
+            or (x == None and y == None and z == None)
+        ):
+            raise ValueError("There must only be one known value")
+
+        for i in range(len(new_point)):
+            if new_point[i] != None:
+                a = i
+        # If a 0 its x, if 1 its y, if its 2 z
+
+        # a = x if x not None else y if y not None else z
+        result = [0, 0, 0]
+        b = (a + 1) % 3
+        c = (a + 2) % 3
+        # This may generate a negative number
+        t = (new_point[a] - point1[a]) / (point2[a] - point1[a])
+        result[b] = point1[b] + t * (point2[b] - point1[b])
+        result[c] = point1[c] + t * (point2[c] - point1[c])
+        result[a] = new_point[a]
+        return tuple(map(round, result))
+
+    def connect(point1, point2):
         x1, y1, z1 = point1
         x2, y2, z2 = point2
         distances = (x1 - x2, y1 - y2, z1 - z2)
@@ -121,38 +158,12 @@ class Cube():
                 point1, point2 = point2, point1
             yield Cube.connect3d(point1, point2, tuple(arg))
 
-    def connect3d(point1: tuple, point2: tuple, new_point: tuple) -> list:
-        # Formula new_point = point1 + t * point2
-        #                     always (0, 0, 0) so we can remove it
-        x, y, z = new_point
-        if (x != None and y != None) or (x != None and z != None) or (y != None and z != None) or (x == None and y == None and z == None):
-            raise (ValueError, "There must only be one known value")
-
-        for i in range(len(new_point)):
-            if new_point[i] != None:
-                a = i
-        # If a 0 its x, if 1 its y, if its 2 z
-
-        # a = x if x not None else y if y not None else z 
-        result = [0, 0, 0]
-        b = (a + 1) % 3
-        c = (a + 2) % 3
-        # This may generate a negative number
-        t = (new_point[a] - point1[a]) / (point2[a] - point1[a])
-        result[b] = point1[b] + t * (point2[b] - point1[b])
-        result[c] = point1[c] + t * (point2[c] - point1[c])
-        result[a] = new_point[a]
-        return tuple(map(round, result))
-
-
     def connect_all(self):
         points = self.get_points()
         all_new_points = []
 
         for point in points:
             connection_points = Cube.find_3_smallest(point, points)
-
-            result_points = []
             for connection_point in connection_points:
                 all_new_points.append(Cube.connect(connection_point, point))
         all_points = []
@@ -160,7 +171,6 @@ class Cube():
             for a in i:
                 all_points.append(a)
         self.write_new_points(all_points)
-
 
     def get_points(self) -> list:
         result = []
@@ -198,11 +208,35 @@ class Cube():
                     s += " ."
             print(f"{s}")
 
+    def get_proyection_string(proyection):
+        result = ""
+        for i in proyection:
+            s = ""
+            for x in i:
+                if type(x) == tuple:
+                    on, depth = x
+                else:
+                    on = x
+                if on:
+                    depth = (
+                        len(symbols)
+                        - 1
+                        - math.floor(depth * len(symbols) / len(proyection))
+                    )
+                    s += f" {symbols[depth]}"
+                else:
+                    s += "  "
+            result += s + "\n"
+        return result
 
-    def rotate(self, angle: int):
-        self.rotation = angle
+    def rotate(self, angle_x: float, angle_y: float, angle_z: float):
+        self.rotation = angle_y
         points = self.get_points()
-        angle = math.radians(angle)
+        angle_x, angle_y, angle_z = (
+            math.radians(angle_x),
+            math.radians(angle_y),
+            math.radians(angle_z),
+        )
 
         def transform(point):
             s = math.floor(self.s / 2)
@@ -212,6 +246,7 @@ class Cube():
                 return 0
             elif point > s:
                 return point - s
+
         def de_transform(point):
             s = math.floor(self.s / 2)
             point = round(point)
@@ -226,11 +261,20 @@ class Cube():
         for point in points:
             x, y, z = point
             x, y, z = transform(x), transform(y), transform(z)
-            new_x = sum([cos(angle) * x, -sin(angle) * y, 0])
-            new_y = sum([0, 0, z])
-            new_z = sum([sin(angle) * x, cos(angle) * y, 0])
 
-            result.append((de_transform(new_x), de_transform(new_y), de_transform(new_z)))
+            x1 = sum([cos(angle_x) * x, -sin(angle_x) * y, 0])
+            y1 = sum([0, 0, z])
+            z1 = sum([sin(angle_x) * x, cos(angle_x) * y, 0])
+
+            x3 = sum([cos(angle_y) * x1, -sin(angle_y) * y1, 0])
+            y3 = sum([0, 0, z1])
+            z3 = sum([sin(angle_y) * x1, cos(angle_y) * y1, 0])
+
+            # x3 = sum([cos(angle_z) * x2, -sin(angle_z) * y2, 0])
+            # y3 = sum([0, 0, z2])
+            # z3 = sum([sin(angle_z) * x2, cos(angle_z) * y2, 0])
+
+            result.append((de_transform(x3), de_transform(y3), de_transform(z3)))
         return result
 
     def proyect(self):
@@ -254,5 +298,13 @@ class Cube():
                         result[x][z] = (1, i)
         return result
 
+
 if __name__ == "__main__":
-    cube = Cube(23)
+    terminal_size = os.get_terminal_size()
+    size = (
+        terminal_size.lines
+        if terminal_size.lines < terminal_size.columns
+        else terminal_size.columns
+    )
+    size = size - 1 - (size % 2)
+    cube = Cube(size)
